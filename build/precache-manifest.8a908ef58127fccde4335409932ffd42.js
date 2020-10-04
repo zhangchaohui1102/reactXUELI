1f0b9ad02563e1baf80c3f4def757bb26e76d2ab
张朝会 <45160850+zhangchaohui1102@users.noreply.github.com>
2020-10-04 10:56:56 +0800

    Add files via upload

---
+++ build/precache-manifest.8a908ef58127fccde4335409932ffd42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b0a7fecf842d3fc8b5103937f38c59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32bd928cdda712f6696",
+    "url": "/static/js/2.1413a434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1413a434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6375186fa9b12599338",
+    "url": "/static/js/main.f9f8ccfa.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
